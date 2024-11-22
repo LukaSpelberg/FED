4a8c7246ddf3c19c4d33b727b6d0fd7934880796
LukaSpelberg <luka.spelberg@hva.nl>
2024-11-22 16:33:11 +0100

    eerste poging css

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,13 @@
-// JavaScript Document
-console.log("hi");
+const deButton = document.querySelector("nav button")
+
+deButton.addEventListener("click", toggleMenu)
+
+function toggleMenu() {  
+    let deNav = document.querySelector("nav")
+    let deHeader = document.querySelector("header section")
+    let deVideo = document.querySelector("header video")
+
+    deNav.classList.toggle("toonMenu")
+  }
+
+
