75079f21020add3607ae9b864515e2af8867f454
LukaSpelberg <luka.spelberg@hva.nl>
2024-11-25 17:18:14 +0100

    push

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,20 @@
 const deButton = document.querySelector("nav button")
+const trailerIframe = document.querySelector("main a:nth-child(1)")
+const videoFrame = document.querySelector("iframe")
 
 deButton.addEventListener("click", toggleMenu)
+
+trailerIframe.addEventListener("mouseenter", autoPlay)
+trailerIframe.addEventListener("mouseleave", autoPlayUit)
+
+
+function autoPlay() {
+  videoFrame.src = "https://www.youtube.com/embed/j56tGNuOqfM?autoplay=1&mute=1"
+}
+
+function autoPlayUit() {
+  videoFrame.src = "https://www.youtube.com/embed/j56tGNuOqfM?si=ZJrihVAnYZP0Ta0G"
+}
 
 function toggleMenu() {  
     let deNav = document.querySelector("nav")
